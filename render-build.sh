97a7a34d929733e89987e3e708106adf83718bef
TejasBedarkar <151560417+TejasBedarkar@users.noreply.github.com>
2026-04-22 11:40:24 +0530

    Refactor render-build.sh for Bench initialization
    Updated the render-build.sh script to initialize the Bench and create the site configuration automatically.

--- render-build.sh
+++ render-build.sh
@@ -4,8 +4,7 @@ set -o errexit
 # 1. Install Bench
 pip install frappe-bench
 
-# 2. Build the Bench for Version 15 
-# We add --skip-backups to prevent the 'crontab' error on Render
+# 2. Initialize the Bench
 bench init frappe-bench \
   --frappe-branch version-15 \
   --python python3.11 \
@@ -14,8 +13,27 @@ bench init frappe-bench \
 
 cd frappe-bench
 
-# 3. Get your custom app
+# 3. Get your custom HR app
 bench get-app https://github.com/TejasBedarkar/Frabbe_Backend.git
 
-# 4. Create the site folder manually
+# 4. Create the site (This generates the necessary config files)
+# Note: This will fail if DB credentials aren't in env vars, 
+# so we use 'force' or manual config creation.
 mkdir -p sites/magna.in
+
+# 5. Create the site_config.json automatically
+cat <<EOF > sites/magna.in/site_config.json
+{
+ "db_name": "$DB_NAME",
+ "db_password": "$DB_PASSWORD",
+ "db_type": "mariadb",
+ "db_host": "$DB_HOST",
+ "db_port": 3306
+}
+EOF
+
+# 6. Set as default site
+echo "magna.in" > sites/current_site.txt
+
+# 7. Build assets
+bench build
