4ec05689970bc7440f808156da90e3f7b64c2b20
TejasBedarkar <151560417+TejasBedarkar@users.noreply.github.com>
2026-04-22 11:48:24 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -5,35 +5,29 @@ set -o errexit
 pip install frappe-bench
 
 # 2. Initialize the Bench
-bench init frappe-bench \
-  --frappe-branch version-15 \
-  --python python3.11 \
-  --skip-redis-config-generation \
-  --no-backups
+bench init frappe-bench --frappe-branch version-15 --python python3.11 --skip-redis-config-generation --no-backups
 
 cd frappe-bench
 
-# 3. Get your custom HR app
+# 3. Get your custom app
 bench get-app https://github.com/TejasBedarkar/Frabbe_Backend.git
 
-# 4. Create the site (This generates the necessary config files)
-# Note: This will fail if DB credentials aren't in env vars, 
-# so we use 'force' or manual config creation.
+# 4. CRITICAL: Create the config file manually
+# This tells Frappe where your database is!
 mkdir -p sites/magna.in
-
-# 5. Create the site_config.json automatically
 cat <<EOF > sites/magna.in/site_config.json
 {
  "db_name": "$DB_NAME",
  "db_password": "$DB_PASSWORD",
  "db_type": "mariadb",
  "db_host": "$DB_HOST",
- "db_port": 3306
+ "db_port": 3306,
+ "full_name": "magna.in"
 }
 EOF
 
-# 6. Set as default site
+# 5. Set the site as the current one
 echo "magna.in" > sites/current_site.txt
 
-# 7. Build assets
-bench build
+# 6. Build assets so the UI loads
+bench build --app frappe
